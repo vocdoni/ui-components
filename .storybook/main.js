e2dd8ef11d6c9eb0b58844837521b8f5d9acf148
selankon <selankon@selankon.xyz>
2022-12-20 15:58:21 +0100

    Add chakra addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@chakra-ui/storybook-addon",
   ],
   "framework": "@storybook/react"
 }
