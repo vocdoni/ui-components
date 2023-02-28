1867451ecc705c448ff1ea9e39060d2ec4e61fb5
Òscar Casajuana <elboletaire@gmail.com>
2023-02-28 18:23:43 +0100

    'chakraize' Balance component

--- packages/components/src/components/Account/Balance.tsx
+++ packages/components/src/components/Account/Balance.tsx
@@ -1,8 +1,8 @@
-import { chakra } from '@chakra-ui/system'
-import { Tag } from '@chakra-ui/tag'
+import { chakra, ChakraComponent } from '@chakra-ui/system'
+import { Tag, TagProps } from '@chakra-ui/tag'
 import { useClientContext } from '../../client'
 
-const _Balance = () => {
+const _Balance = (props: ChakraComponent<'span', TagProps>) => {
   const { balance } = useClientContext()
 
   if (balance < 0) {
@@ -10,7 +10,15 @@ const _Balance = () => {
     return null
   }
 
-  return <Tag size='sm' colorScheme='teal'>{balance} votokens</Tag>
+  let color = 'teal'
+  if (balance < 50 && balance > 20) {
+    color = 'yellow'
+  } else if (balance <= 20) {
+    color = 'red'
+  }
+
+
+  return <Tag size='sm' colorScheme={color} {...props}>{balance} votokens</Tag>
 }
 
 export const Balance = chakra(_Balance)
