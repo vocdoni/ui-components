0ab092c408e920c864e441836af52fd4b8a9c46d
Òscar Casajuana <elboletaire@gmail.com>
2023-05-02 17:33:08 +0200

    minor code change following ritmo's suggestion

--- packages/chakra-components/src/components/Election/Schedule.tsx
+++ packages/chakra-components/src/components/Election/Schedule.tsx
@@ -8,21 +8,16 @@ export type ElectionScheduleProps = HeadingProps & {
   format?: string
 }
 
-export const ElectionSchedule = forwardRef<ElectionScheduleProps, 'h2'>(({ format, ...rest }, ref) => {
+export const ElectionSchedule = forwardRef<ElectionScheduleProps, 'h2'>(({ format = 'd-L-y HH:mm', ...rest }, ref) => {
   const styles = useMultiStyleConfig('ElectionSchedule', rest)
   const { election } = useElection()
-
-  let f = format
-  if (!f) {
-    f = 'd-L-y HH:mm'
-  }
 
   if (!election) return null
 
   return (
     <chakra.h2 __css={styles} {...rest}>
-      Voting period {dformat(new Date(election.startDate), f)} ~&nbsp;
-      {dformat(new Date(election.endDate), f)}
+      Voting period {dformat(new Date(election.startDate), format)} ~&nbsp;
+      {dformat(new Date(election.endDate), format)}
     </chakra.h2>
   )
 })
