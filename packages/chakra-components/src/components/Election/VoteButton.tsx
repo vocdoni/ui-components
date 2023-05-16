ab2dc0a07044b8e3201476c34338ae0fcecc26f4
Òscar Casajuana <elboletaire@gmail.com>
2023-05-16 18:36:54 +0200

    Fix vote button loading on autoUpdates
    closes #38

--- packages/chakra-components/src/components/Election/VoteButton.tsx
+++ packages/chakra-components/src/components/Election/VoteButton.tsx
@@ -3,7 +3,7 @@ import { ElectionStatus } from '@vocdoni/sdk'
 import { useElection } from './Election'
 
 export const VoteButton = (props: ButtonProps) => {
-  const { signer, loading, voting, ConnectButton, isAbleToVote, election, voted, trans } = useElection()
+  const { signer, voting, ConnectButton, isAbleToVote, election, voted, trans } = useElection()
   const isDisabled = !signer || !isAbleToVote || election?.status !== ElectionStatus.ONGOING
 
   if (!signer && ConnectButton) {
@@ -11,13 +11,7 @@ export const VoteButton = (props: ButtonProps) => {
   }
 
   return (
-    <Button
-      type='submit'
-      {...props}
-      form='election-questions-form'
-      isDisabled={isDisabled}
-      isLoading={loading || voting}
-    >
+    <Button type='submit' {...props} form='election-questions-form' isDisabled={isDisabled} isLoading={voting}>
       {voted && isAbleToVote ? trans('vote.button_update') : trans('vote.button')}
     </Button>
   )
