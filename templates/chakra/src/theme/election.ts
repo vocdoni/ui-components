c98d137cad20cdb3c7739c4a0da9a5366d38c33a
Òscar Casajuana <elboletaire@gmail.com>
2023-05-22 13:32:52 +0200

    Minor style change for chakra template

--- templates/chakra/src/theme/election.ts
+++ templates/chakra/src/theme/election.ts
@@ -2,18 +2,22 @@ import { ComponentStyleConfig } from '@chakra-ui/react'
 
 export const ElectionHeader: ComponentStyleConfig = {
   baseStyle: {
-    minHeight: '3em',
+    '& + h1': {
+      minHeight: '3em',
+    },
   },
 }
 
 export const ElectionTitle: ComponentStyleConfig = {
   baseStyle: ({ colorMode }) => ({
-    paddingY: '.5em',
-    position: 'relative',
-    marginTop: '-2.5em',
-    background:
-      colorMode === 'dark'
-        ? 'linear-gradient(180deg, rgba(0,0,0,0.50) 0%, rgba(0,0,0,0.25) 35%, rgba(0,0,0,0) 100%)'
-        : 'linear-gradient(180deg, rgba(255,255,255,0.6) 0%, rgba(255,255,255,1) 30%)',
+    'img + &': {
+      paddingY: '.5em',
+      position: 'relative',
+      marginTop: '-2.5em',
+      background:
+        colorMode === 'dark'
+          ? 'linear-gradient(180deg, rgba(0,0,0,0.50) 0%, rgba(0,0,0,0.25) 35%, rgba(0,0,0,0) 100%)'
+          : 'linear-gradient(180deg, rgba(255,255,255,0.6) 0%, rgba(255,255,255,1) 30%)',
+    },
   }),
 }
