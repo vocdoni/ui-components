caec4d984f4e4f8aabc5f502533f5636dd2b09ea
Òscar Casajuana <elboletaire@gmail.com>
2023-11-20 18:09:55 +0100

    Remove unnecessary changes (for now)

--- packages/chakra-components/src/components/Account/Signup/Signer.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@chakra-ui/button'
-import { useClient } from '@vocdoni/react-providers'
-
-export const AccountSignupSigner = () => {
-  const { localize } = useClient()
-
-  return <Button>{localize('signup.signer.sign')}</Button>
-}
