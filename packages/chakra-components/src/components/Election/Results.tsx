f00f724cf80d9c89c506a9bf293e7eacac2ed6bb
Òscar Casajuana <elboletaire@underave.net>
2024-01-17 18:03:02 +0100

    POC Multi-choice
    - Can vote on multi-choice elections
    - Shows an abstain option for each abstain possible (repeated when
      voting for now)
    - Abstains are grouped in results

--- packages/chakra-components/src/components/Election/Results.tsx
+++ packages/chakra-components/src/components/Election/Results.tsx
@@ -1,8 +1,15 @@
 import { Box, Flex, Text } from '@chakra-ui/layout'
 import { Progress } from '@chakra-ui/progress'
-import { chakra, ChakraProps, useMultiStyleConfig } from '@chakra-ui/system'
+import { ChakraProps, chakra, useMultiStyleConfig } from '@chakra-ui/system'
 import { useClient, useDatesLocale, useElection } from '@vocdoni/react-providers'
-import { ElectionStatus, formatUnits, InvalidElection } from '@vocdoni/sdk'
+import {
+  ElectionResultsTypeNames,
+  ElectionStatus,
+  IChoice,
+  InvalidElection,
+  PublishedElection,
+  formatUnits,
+} from '@vocdoni/sdk'
 import { format } from 'date-fns'
 
 const percent = (result: number, total: number) => ((Number(result) / total) * 100 || 0).toFixed(0) + '%'
@@ -27,41 +34,68 @@ export const ElectionResults = (props: ChakraProps) => {
     )
   }
 
+  console.log('election:', election)
   const decimals = (election.meta as any)?.token?.decimals || 0
   const totals = election?.questions
     .map((el) => el.choices.reduce((acc, curr) => acc + Number(curr.results), 0))
     .map((votes: number) => results(votes, decimals))
-
+  console.log('totals:', totals)
   return (
     <Flex sx={styles.wrapper} {...props}>
-      {election?.questions.map((q: any, idx: number) => (
-        <chakra.div key={idx} sx={styles.question}>
-          <chakra.div sx={styles.header}>
-            <Text sx={styles.title}>{localize('results.title', { title: q.title.default })}</Text>
+      {election?.questions.map((q: any, idx: number) => {
+        const choices = electionChoices(election, q.choices, localize('vote.abstain'))
+        console.log('choices:', choices)
+        return (
+          <chakra.div key={idx} sx={styles.question}>
+            <chakra.div sx={styles.header}>
+              <Text sx={styles.title}>{localize('results.title', { title: q.title.default })}</Text>
+            </chakra.div>
+            <chakra.div sx={styles.body}>
+              {choices.map((c: any, i: number) => (
+                <Box key={i}>
+                  {totals && (
+                    <>
+                      <Text sx={styles.choiceTitle}>{c.title.default}</Text>
+                      <Text sx={styles.choiceVotes}>
+                        {localize('results.votes', {
+                          votes: results(c.results, decimals) || 0,
+                          percent: percent(results(c.results, decimals), totals[idx]),
+                        })}
+                      </Text>
+                      <Progress
+                        sx={styles.progress}
+                        value={((Number(c.results) / totals[idx]) * 100) / 10 ** decimals || 0}
+                      />
+                    </>
+                  )}
+                </Box>
+              ))}
+            </chakra.div>
           </chakra.div>
-          <chakra.div sx={styles.body}>
-            {election?.questions[idx].choices.map((c: any, i: number) => (
-              <Box key={i}>
-                {totals && (
-                  <>
-                    <Text sx={styles.choiceTitle}>{c.title.default}</Text>
-                    <Text sx={styles.choiceVotes}>
-                      {localize('results.votes', {
-                        votes: results(c.results, decimals) || 0,
-                        percent: percent(results(c.results, decimals), totals[idx]),
-                      })}
-                    </Text>
-                    <Progress
-                      sx={styles.progress}
-                      value={((Number(c.results) / totals[idx]) * 100) / 10 ** decimals || 0}
-                    />
-                  </>
-                )}
-              </Box>
-            ))}
-          </chakra.div>
-        </chakra.div>
-      ))}
+        )
+      })}
     </Flex>
   )
+}
+
+const electionChoices = (election: PublishedElection, choices: IChoice[], abstainLabel: string) => {
+  const nchoices = [...choices]
+  if (election.resultsType.name === ElectionResultsTypeNames.MULTIPLE_CHOICE) {
+    console.log('received results:', election.results)
+    const results = election.results[0].map((val, key) => parseInt(val, 10) + parseInt(election.results[1][key], 10))
+    for (const k in nchoices) {
+      nchoices[k].results = results[parseInt(nchoices[k].value, 10)]
+    }
+    const abstain = {
+      title: {
+        default: abstainLabel,
+      },
+      results: 0,
+    }
+    for (const id of election.resultsType.properties.abstainValues) {
+      abstain.results += parseInt(results[id], 10)
+    }
+    nchoices.push(abstain)
+  }
+  return nchoices
 }
