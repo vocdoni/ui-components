78aac6c859a34b22e6b561f8a169176c88291105
Òscar Casajuana <elboletaire@gmail.com>
2024-04-03 11:54:26 +0200

    Fix window is undefined
    closes #155

--- packages/react-providers/src/worker/webWorker.ts
+++ packages/react-providers/src/worker/webWorker.ts
@@ -1,7 +1,8 @@
 export const createWebWorker = (worker: any) => {
   const code = worker.toString()
   const blob = new Blob(['(' + code + ')()'])
-  const workerURL = (window as any).MockedWindowURL || window.URL || window.webkitURL
-  const blobURL = workerURL.createObjectURL(blob)
+  const url =
+    typeof window === 'undefined' ? import.meta.url : (window as any).MockedWindowURL || window.URL || window.webkitURL
+  const blobURL = url.createObjectURL(blob)
   return new Worker(blobURL)
 }
