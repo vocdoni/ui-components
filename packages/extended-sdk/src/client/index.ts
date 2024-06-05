94b74230c21193db85c8ae74ef6ffaf7c2055657
Òscar Casajuana <elboletaire@gmail.com>
2024-06-05 10:11:00 +0200

    Manually add non-exported SDK type

--- packages/extended-sdk/src/client/index.ts
+++ packages/extended-sdk/src/client/index.ts
@@ -1,18 +1,15 @@
-import {
-  AccountAPI,
-  ChainAPI,
-  ClientOptions,
-  ElectionAPI,
-  IChainBlockInfoResponse,
-  VocdoniSDKClient,
-  VoteAPI,
-} from '@vocdoni/sdk'
+import { AccountAPI, ChainAPI, ElectionAPI, IChainBlockInfoResponse, VocdoniSDKClient, VoteAPI } from '@vocdoni/sdk'
+
+// Sorry for this, but we need to publish and the type isn't yet exported from the SDK...
+// TODO: remove when it's properly exported
+interface IElectionVotesCountResponse {
+  /**
+   * The number of votes
+   */
+  count: number
+}
 
 export class ExtendedSDKClient extends VocdoniSDKClient {
-  constructor(opts: ClientOptions) {
-    super(opts)
-  }
-
   txInfo = (txHash: string) => ChainAPI.txInfo(this.url, txHash)
   txInfoByBlock = (blockHeight: number, txIndex: number) => ChainAPI.txInfoByBlock(this.url, blockHeight, txIndex)
   txList = (page?: number) => ChainAPI.txList(this.url, page)
@@ -26,7 +23,8 @@ export class ExtendedSDKClient extends VocdoniSDKClient {
   blockByHash = (hash: string) => ChainAPI.blockByHash(this.url, hash)
   voteInfo = (voteId: string) => VoteAPI.info(this.url, voteId)
   electionVotesList = (electionId: string, page?: number) => ElectionAPI.votesList(this.url, electionId, page)
-  electionVotesCount = (electionId: string) => ElectionAPI.votesCount(this.url, electionId)
+  electionVotesCount = (electionId: string) =>
+    ElectionAPI.votesCount(this.url, electionId) as Promise<IElectionVotesCountResponse>
   electionKeys = (electionId: string) => ElectionAPI.keys(this.url, electionId)
   electionList = (page: number, electionId?: string, organizationId?: string, status?: any, withResults?: boolean) =>
     ElectionAPI.electionsList(this.url, page, {
