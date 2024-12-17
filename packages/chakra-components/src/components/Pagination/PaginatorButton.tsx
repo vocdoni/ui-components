fc591bc4bd38e80db1b5c8444107ae2b666d1cfd
Òscar Casajuana <elboletaire@gmail.com>
2024-12-17 10:03:21 +0100

    Remove unfinished (and unrelated) code

--- packages/chakra-components/src/components/Pagination/PaginatorButton.tsx
+++ packages/chakra-components/src/components/Pagination/PaginatorButton.tsx
@@ -4,8 +4,3 @@ export const PageButton = forwardRef<ButtonProps, 'div'>((props, ref) => {
   const styles = useStyleConfig('PageButton', props)
   return <Button sx={styles} ref={ref} {...props} />
 })
-
-export const NextPage = forwardRef<ButtonProps, 'div'>((props, ref) => {
-  const styles = useStyleConfig('PageButton', { ...props, variant: 'navigation' })
-  return <Button sx={styles} ref={ref} {...props} />
-})
