63ebdd13f204aa1659001529cc9b185998a72ba6
emmdim <emmdim@users.noreply.github.com>
2025-02-05 12:40:27 +0100

    feat: saas oauth

--- packages/rainbowkit-wallets/src/index.ts
+++ packages/rainbowkit-wallets/src/index.ts
@@ -1,5 +1,6 @@
 import { inputsWallet } from './inputsWallet'
 import { oAuthWallet } from './oAuthWallet'
 import { privateKeyWallet } from './privateKeyWallet'
+import { saasOAuthWallet } from './saasOauthWallet'
 
-export { inputsWallet, oAuthWallet, privateKeyWallet }
+export { inputsWallet, oAuthWallet, privateKeyWallet, saasOAuthWallet }
